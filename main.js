276f971fe77c42ca6f7707ba9c9eebec4a224f9d
Shahzod12 <104401201+Shahzod12@users.noreply.github.com>
2023-01-26 19:35:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+song = "";
+
+function preload(params)
+{
+    song = loadSound("music.mp3");
+}
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+image(video, 0, 0, 600, 500);
+}
+
+
+
+function play(params) 
+{
+    song.play();
+}
